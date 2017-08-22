f69099038f64a49eb8a3e0a00f580dfdc0d55c2c
Dmitry Kolinchuk <dmko@sitecore.net>
2017-08-22 11:22:59 +0300

    Fixes image disappearing after changing its size in Rich Text Editor

---
+++ src/Sitecore.Support.451366/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.451366")]
+[assembly: AssemblyProduct("Sitecore.Support.451366")]
+[assembly: ComVisible(false)]
